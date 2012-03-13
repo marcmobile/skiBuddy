76807ad78cbc7f720f6f5b73a4723c522ec8b4a5
Lui Marciante <lui@Luis-MacBook-Air.local>
2012-03-12 21:32:56 -0700

    Initial Commit

---
+++ skiBuddy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  skiBuddy
+//
+//  Created by Lui Marciante on 12-03-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ skiBuddy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  skiBuddy
+//
+//  Created by Lui Marciante on 12-03-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
